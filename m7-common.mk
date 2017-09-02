8ac698af77af62b8f4a864eb38c37a9d781ee8ce
Ta Thanh Lam <tathanhlam66@gmail.com>
2017-09-03 05:44:13 +0700

    m7-common: hidl: Add USB HAL interfaces package

--- m7-common.mk
+++ m7-common.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
 
+# USB HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
